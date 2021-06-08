2ad54728ed13894dd73fff4fbe66f514eb95d551
Pavel Pospolit <pavel.pospolit@Telekom.de>
2021-06-08 10:56:11 +0200

    Sign up with DB

--- src/com/trainee/ReservierungssystemFX/actions/Random_Number_Generator.java
+++ src/com/trainee/ReservierungssystemFX/actions/Random_Number_Generator.java
@@ -1,24 +1,46 @@
 package com.trainee.ReservierungssystemFX.actions;
 
 /*Creates a random number for 2 cases:
-*employee (Mitarbeiter) and booking (Reservierung)
-*/
+ *employee (Mitarbeiter) and booking (Reservierung)
+ */
 
-import com.trainee.ReservierungssystemFX.resources.CreateData;
+import com.trainee.ReservierungssystemFX.resources.Constants;
+
+import java.sql.*;
 
 public class Random_Number_Generator {
-    public int MaRandomNumber(){
-        int iRand;
-        do {
-            iRand = (int) (Math.random() * 99999999) + 10000000;
-        } while (CreateData.getAllEmployees().containsKey(("MA"+iRand)));
+    public int MaRandomNumber() {
+        int iRand = 0;
+        try (Connection con = DriverManager.getConnection(Constants.sql_url); Statement stmt = con.createStatement();) {
+            while (true) {
+                iRand = (int) (Math.random() * 99999999) + 10000000;
+                String SQL = "SELECT EmployeeID FROM dbo.Employees WHERE EmployeeID like'" + iRand + "'";
+                ResultSet rs = stmt.executeQuery(SQL);
+                if (!rs.next()) {
+                    break;
+                }
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return iRand;
     }
-    public  int ResRandomNumber(){
-        int iRand;
-        do {
-            iRand = (int) (Math.random() * 99999999) + 10000000;
-        } while (CreateData.getAllReservations().containsKey(("BU"+iRand)));
+
+    public int ResRandomNumber() {
+        int iRand = 0;
+        try (Connection con = DriverManager.getConnection(Constants.sql_url); Statement stmt = con.createStatement();) {
+            while (true) {
+                iRand = (int) (Math.random() * 99999999) + 10000000;
+                String SQL = "SELECT ReservationID FROM dbo.Reservations WHERE ReservationID like'" + iRand + "'";
+                ResultSet rs = stmt.executeQuery(SQL);
+                if (!rs.next()) {
+                    break;
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return iRand;
     }
 }
--- src/com/trainee/ReservierungssystemFX/controller/sign_in_controller.java
+++ src/com/trainee/ReservierungssystemFX/controller/sign_in_controller.java
@@ -2,6 +2,7 @@ package com.trainee.ReservierungssystemFX.controller;
 
 import com.trainee.ReservierungssystemFX.Classes.Mitarbeiter;
 import com.trainee.ReservierungssystemFX.actions.Random_Number_Generator;
+import com.trainee.ReservierungssystemFX.resources.Constants;
 import com.trainee.ReservierungssystemFX.resources.CreateData;
 import com.trainee.ReservierungssystemFX.resources.FrequentlyUsedButtons;
 import javafx.fxml.FXML;
@@ -16,6 +17,7 @@ import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.sql.*;
 
 public class sign_in_controller {
 
@@ -49,76 +51,22 @@ public class sign_in_controller {
     }
 
     public void logInOnMouseClicked(MouseEvent mouseEvent) throws IOException {
-        bEmail = true;
+        bEmail = false;
         bPassword = true;
         sEmail = textFieldEmail.getText();
         sPassword = textFieldPassword.getText();
-        for (String key : CreateData.getAllEmployees().keySet()) {
-            if (CreateData.getEmployee(key).getsMaName().equals(sEmail)) {
 
-                bEmail = false;
+        try (Connection con = DriverManager.getConnection(Constants.sql_url); Statement stmt = con.createStatement();) {
+            String SQL = "SELECT * FROM dbo.Employees WHERE Emailaddress like '" + sEmail + "'";
+            ResultSet rs = stmt.executeQuery(SQL);
+            if (!rs.next()) {
+                bEmail = true;
+            } else {
                 errorWindowDoubleEmail.setText("Mitarbeiter " + sEmail + " bereits vorhanden!\nBitte Einloggen!");
-                break;
-            }
-        }
-        if (textFieldEmail.getText().isEmpty()) {
-            errorWindowDoubleEmail.setText("Bitte Emailadresse eingeben!");
-            bEmail = false;
-        }
-        if (textFieldPassword.getText().isEmpty()) {
-            errorWindowPassword.setText("Bitte ein Password eingeben!");
-            bPassword = false;
-        } else if (textFieldPassword2.getText().isEmpty()) {
-            errorWindowPassword2.setText("Bitte ein Password eingeben!");
-            bPassword = false;
-        } else if ((textFieldPassword.getText().length() < 8)) {
-            errorWindowPassword.setText("Passwort muss mindestens 8 Zeichen lang sein!");
-            bPassword = false;
-        } else if (!sPassword.equals(textFieldPassword2.getText())) {
-            bPassword = false;
-            errorWindowPassword.setText("Passwörter stimmen nicht überein!");
-            errorWindowPassword2.setText("Passwörter stimmen nicht überein!");
-        } else if ((!sEmail.contains("@")) && (!sEmail.contains("."))) {
-            bEmail = false;
-            errorWindowDoubleEmail.setText("Bitte gültige Emailadresse benutzen!");
-        }
-
-        if (bEmail && bPassword) {
-            Random_Number_Generator rand = new Random_Number_Generator();
-            int iRand = rand.MaRandomNumber();
-            String employeeNumber = "MA" + iRand;
-            Mitarbeiter employee = new Mitarbeiter(sEmail,
-                    employeeNumber,
-                    sPassword
-            );
-            CreateData.addEmployee(employee.getsMitarbeiterID(), employee);
-
-            Alert alert = new Alert(Alert.AlertType.INFORMATION);
-            alert.setTitle("ERFOLG!");
-            alert.setContentText("Mitarbeiter erfolgreich hinzugefügt!");
-            alert.showAndWait();
-            FrequentlyUsedButtons.goToSignIn(mouseEvent);
-        }
-
-    }
-
-    public void signUpEnter(KeyEvent keyEvent) throws IOException {
-        if(keyEvent.getCode()== KeyCode.ENTER){
-            bEmail = true;
-            bPassword = true;
-            sEmail = textFieldEmail.getText();
-            sPassword = textFieldPassword.getText();
-            for (String key : CreateData.getAllEmployees().keySet()) {
-                if (CreateData.getEmployee(key).getsMaName().equals(sEmail)) {
-
-                    bEmail = false;
-                    errorWindowDoubleEmail.setText("Mitarbeiter " + sEmail + " bereits vorhanden!\nBitte Einloggen!");
-                    break;
-                }
+                bEmail = false;
             }
             if (textFieldEmail.getText().isEmpty()) {
                 errorWindowDoubleEmail.setText("Bitte Emailadresse eingeben!");
-                bEmail = false;
             }
             if (textFieldPassword.getText().isEmpty()) {
                 errorWindowPassword.setText("Bitte ein Password eingeben!");
@@ -137,23 +85,89 @@ public class sign_in_controller {
                 bEmail = false;
                 errorWindowDoubleEmail.setText("Bitte gültige Emailadresse benutzen!");
             }
-
             if (bEmail && bPassword) {
+                errorWindowPassword.setText("");
+                errorWindowPassword2.setText("");
+                errorWindowDoubleEmail.setText("");
                 Random_Number_Generator rand = new Random_Number_Generator();
                 int iRand = rand.MaRandomNumber();
-                String employeeNumber = "MA" + iRand;
-                Mitarbeiter employee = new Mitarbeiter(sEmail,
-                        employeeNumber,
-                        sPassword
-                );
-                CreateData.addEmployee(employee.getsMitarbeiterID(), employee);
-
+                String insert = "insert into Employees values(?,?,?)";
+                PreparedStatement insertstmt = con.prepareStatement(insert);
+                insertstmt.setInt(1, iRand);
+                insertstmt.setString(2, sEmail);
+                insertstmt.setString(3, sPassword);
+                insertstmt.execute();
                 Alert alert = new Alert(Alert.AlertType.INFORMATION);
                 alert.setTitle("ERFOLG!");
                 alert.setContentText("Mitarbeiter erfolgreich hinzugefügt!");
                 alert.showAndWait();
-                FrequentlyUsedButtons.goToSignInKey(keyEvent);
+                FrequentlyUsedButtons.goToSignIn(mouseEvent);
             }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void signUpEnter(KeyEvent keyEvent) throws IOException {
+        if (keyEvent.getCode() == KeyCode.ENTER) {
+            bEmail = false;
+            bPassword = true;
+            sEmail = textFieldEmail.getText();
+            sPassword = textFieldPassword.getText();
+
+            try (Connection con = DriverManager.getConnection(Constants.sql_url); Statement stmt = con.createStatement();) {
+                String SQL = "SELECT * FROM dbo.Employees WHERE Emailaddress like '" + sEmail + "'";
+                ResultSet rs = stmt.executeQuery(SQL);
+                if (!rs.next()) {
+                    bEmail = true;
+                } else {
+                    errorWindowDoubleEmail.setText("Mitarbeiter " + sEmail + " bereits vorhanden!\nBitte Einloggen!");
+                    bEmail = false;
+                }
+                if (textFieldEmail.getText().isEmpty()) {
+                    errorWindowDoubleEmail.setText("Bitte Emailadresse eingeben!");
+                    bEmail = false;
+                }
+                if (textFieldPassword.getText().isEmpty()) {
+                    errorWindowPassword.setText("Bitte ein Password eingeben!");
+                    bPassword = false;
+                } else if (textFieldPassword2.getText().isEmpty()) {
+                    errorWindowPassword2.setText("Bitte ein Password eingeben!");
+                    bPassword = false;
+                } else if ((textFieldPassword.getText().length() < 8)) {
+                    errorWindowPassword.setText("Passwort muss mindestens 8 Zeichen lang sein!");
+                    bPassword = false;
+                } else if (!sPassword.equals(textFieldPassword2.getText())) {
+                    bPassword = false;
+                    errorWindowPassword.setText("Passwörter stimmen nicht überein!");
+                    errorWindowPassword2.setText("Passwörter stimmen nicht überein!");
+                } else if ((!sEmail.contains("@")) && (!sEmail.contains("."))) {
+                    bEmail = false;
+                    errorWindowDoubleEmail.setText("Bitte gültige Emailadresse benutzen!");
+                }
+                if (bEmail && bPassword) {
+                    errorWindowPassword.setText("");
+                    errorWindowPassword2.setText("");
+                    errorWindowDoubleEmail.setText("");
+                    Random_Number_Generator rand = new Random_Number_Generator();
+                    int iRand = rand.MaRandomNumber();
+                    String insert = "insert into Employees values(?,?,?)";
+                    PreparedStatement insertstmt = con.prepareStatement(insert);
+                    insertstmt.setInt(1, iRand);
+                    insertstmt.setString(2, sEmail);
+                    insertstmt.setString(3, sPassword);
+                    insertstmt.execute();
+                    Alert alert = new Alert(Alert.AlertType.INFORMATION);
+                    alert.setTitle("ERFOLG!");
+                    alert.setContentText("Mitarbeiter erfolgreich hinzugefügt!");
+                    alert.showAndWait();
+                    FrequentlyUsedButtons.goToSignInKey(keyEvent);
+                }
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+
         }
     }
 }
+
